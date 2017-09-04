155ad2573df36b7bc4f9de46b3a4e1d60f9ab048
Nacho Caballero <nachocab@gmail.com>
2017-09-04 17:29:23 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Select Paragraph
+
+Expand the current selection until it hits a whitespace boundary (port of `expand_selection_to_paragraph` from Sublime Text).
+
+## Features
+
+\!\[feature X\]\(images/feature-x.png\)
+
+> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+
+## Extension Settings
+
+Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
+
+For example:
+
+This extension contributes the command `extension.selectParagraph` and the keybinding `ctrl + p`
+
---
+++ extension.js
@@ -0,0 +1,30 @@
+const vscode = require('vscode');
+
+function activate(context) {
+  const selectParagraph = vscode.commands.registerCommand('select-paragraph.selectParagraph', function () {
+    const editor = vscode.window.activeTextEditor;
+    let startLine = editor.selection.start.line;
+    let endLine = editor.selection.end.line;
+
+    while (startLine > 0 && !editor.document.lineAt(startLine - 1).isEmptyOrWhitespace) {
+      startLine -= 1;
+    }
+    
+    while (endLine < editor.document.lineCount && !editor.document.lineAt(endLine + 1).isEmptyOrWhitespace) {
+      endLine += 1;
+    }
+
+    // const startCharacter = editor.document.lineAt(startLine).firstNonWhitespaceCharacterIndex;
+    const startCharacter = 0;
+    const endCharacter = editor.document.lineAt(endLine).text.length;
+
+    editor.selection = new vscode.Selection(startLine, startCharacter, endLine, endCharacter);
+  });
+
+  context.subscriptions.push(selectParagraph);
+}
+exports.activate = activate;
+
+function deactivate() {
+}
+exports.deactivate = deactivate;
